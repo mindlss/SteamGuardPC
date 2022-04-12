da795f2d11564a946ce3efd8b35439854312f8c6
vard05 <68402933+vard05@users.noreply.github.com>
2022-04-13 00:20:21 +0300

    Add files via upload

---
+++ C#/accountLinker/accountLinker/Program.cs
@@ -0,0 +1,158 @@
+﻿using Newtonsoft.Json;
+using SteamAuth;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace accountLinker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            signIn(args[0], args[1]);
+        }
+
+        private static void signIn(String loginStr, String passStr)
+        {
+            var roamingDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/steam-guard/accounts/";
+
+
+            var userLogin = new UserLogin(loginStr, passStr);
+
+            LoginResult response = LoginResult.BadCredentials;
+            var emcode = "";
+
+            while ((response = userLogin.DoLogin()) != LoginResult.LoginOkay)
+            {
+                switch (response)
+                {
+                    case LoginResult.NeedEmail:
+
+                        sendLine("1@INPUT@EMAIL");
+
+                        String emailcdc = Console.ReadLine();
+
+                        userLogin.EmailCode = emailcdc;
+                        emcode = emailcdc;
+                        break;
+
+                    case LoginResult.NeedCaptcha:
+
+                        sendLine("1@ERR@CAPTCHA");
+
+                        return;
+
+                    case LoginResult.Need2FA:
+
+                        sendLine("1@ERR@2FA");
+
+                        return;
+
+                    case LoginResult.BadRSA:
+
+                        sendLine("1@ERR1");
+
+                        return;
+
+                    case LoginResult.BadCredentials:
+
+                        sendLine("1@ERR@LOP");
+
+                        return;
+
+                    case LoginResult.TooManyFailedLogins:
+
+                        sendLine("1@ERR@TOOMANY");
+
+                        return;
+
+                    case LoginResult.GeneralFailure:
+
+                        sendLine("1@ERR@LOP");
+
+                        return;
+                }
+            }
+
+            SessionData session = userLogin.Session;
+            AuthenticatorLinker linker = new AuthenticatorLinker(session);
+            AuthenticatorLinker.LinkResult linkResponse = AuthenticatorLinker.LinkResult.GeneralFailure;
+
+            while ((linkResponse = linker.AddAuthenticator()) != AuthenticatorLinker.LinkResult.AwaitingFinalization)
+            {
+                switch (linkResponse)
+                {
+                    case AuthenticatorLinker.LinkResult.MustProvidePhoneNumber:
+
+                        sendLine("1@ERR@PHONE");
+
+                        return;
+                }
+            }
+
+            string json = JsonConvert.SerializeObject(linker.LinkedAccount, Formatting.Indented);
+            var path = roamingDir + loginStr + ".json";
+
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, json);
+            }
+            else
+            {
+                File.Delete(path);
+                File.WriteAllText(path, json);
+            }
+
+            var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(path));
+            values.Add("password", passStr);
+            json = JsonConvert.SerializeObject(values, Formatting.Indented);
+
+            File.Delete(path);
+            File.WriteAllText(path, json);
+
+            AuthenticatorLinker.FinalizeResult finalizeResponse = AuthenticatorLinker.FinalizeResult.GeneralFailure;
+
+            while (finalizeResponse != AuthenticatorLinker.FinalizeResult.Success)
+            {
+
+                sendLine("2@INPUT@SMS");
+
+                string smsCode = Console.ReadLine();
+
+                finalizeResponse = linker.FinalizeAddAuthenticator(smsCode);
+
+                switch (finalizeResponse)
+                {
+                    case AuthenticatorLinker.FinalizeResult.BadSMSCode:
+
+                        sendLine("2@ERR@SMS");
+
+                        continue;
+
+                    case AuthenticatorLinker.FinalizeResult.UnableToGenerateCorrectCodes:
+
+                        sendLine("2@ERR");
+
+                        return;
+
+                    case AuthenticatorLinker.FinalizeResult.GeneralFailure:
+
+                        sendLine("2@ERR");
+
+                        return;
+                }
+
+            }
+
+            sendLine("SUCCESS");
+
+        }
+
+        private static void sendLine(String code)
+        {
+            Console.WriteLine(code);
+            Console.Out.Flush();
+        }
+    }
+}
---
+++ C#/accountLinker/accountLinker/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("accountLinker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("accountLinker")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("378035ce-9bc1-401e-9014-d77c6bc6b62e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
